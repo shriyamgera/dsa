f1852903f524a9a680388130837abf100996f983
shriyamgera <64017939+shriyamgera@users.noreply.github.com>
2025-04-10 04:05:28 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (87.70%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,27 +1,53 @@
 class Solution {
 
 private:
-    void findPermutation(vector<int>&ds,vector<bool>&freq,vector<int>& nums,vector<vector<int>>&ans){
-        if(ds.size()==nums.size()){
-            ans.push_back(ds);
-            return;
-        }
-        for(int i=0;i<nums.size();i++){
-            if(!freq[i]){
-                freq[i]=true;
-                ds.push_back(nums[i]);
-                findPermutation(ds,freq,nums,ans);
-                ds.pop_back();
-                freq[i]=false;
-            }
-        }
+    void findPermutation(vector<int>& nums,int currIndex,vector<vector<int>>&ans){
+    if(currIndex==nums.size()){
+        ans.push_back(nums);
+        return;
     }
+    for(int i=currIndex;i<nums.size();i++){
+        swap(nums[i],nums[currIndex]);
+        findPermutation(nums,currIndex+1,ans);
+        swap(nums[i],nums[currIndex]);
+    }
+}
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
-        vector<int>ds;
-        vector<bool>freq(nums.size());
-        findPermutation(ds,freq,nums,ans);
+        int currIndex=0;
+        findPermutation(nums,currIndex,ans);
         return ans;
     }
 };
+
+
+
+// class Solution {
+
+// private:
+//     void findPermutation(vector<int>&ds,vector<bool>&freq,vector<int>& nums,vector<vector<int>>&ans){
+//         if(ds.size()==nums.size()){
+//             ans.push_back(ds);
+//              ds.pop_back();
+//             return;
+//         }
+//         for(int i=0;i<nums.size();i++){
+//             if(!freq[i]){
+//                 freq[i]=true;
+//                 ds.push_back(nums[i]);
+//                 findPermutation(ds,freq,nums,ans);
+               
+//                 freq[i]=false;
+//             }
+//         }
+//     }
+// public:
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<vector<int>>ans;
+//         vector<int>ds;
+//         vector<bool>freq(nums.size());
+//         findPermutation(ds,freq,nums,ans);
+//         return ans;
+//     }
+// };
