02d13f0354a07b3247c13f3d8e2af902d1c4cd0c
shriyamgera <64017939+shriyamgera@users.noreply.github.com>
2025-06-17 00:43:30 +0530

    Time: 32 ms (21.64%) | Memory: 32.4 MB (15.33%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,16 +1,45 @@
 class Solution {
 public:
+    string encodeString(string str){
+        vector<int> encodedArr(26,0);
+        for(char c:str){
+            encodedArr[c - 'a']++;
+        }
+        string encodedStr = "";
+        for(int i=0; i<encodedArr.size();i++){
+            if(encodedArr[i] !=0){
+                encodedStr += char('a'+i) + to_string(encodedArr[i]);
+            }
+        }
+        return encodedStr;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>>mp;
-        vector<vector<string>> ans;
-        for(int i = 0; i<strs.size();i++){
-            string sortedItem = strs[i];
-            sort(sortedItem.begin(), sortedItem.end());
-            mp[sortedItem].push_back(strs[i]);
+        for(int i = 0; i<strs.size(); i++){
+            string encodedString = encodeString(strs[i]);
+            mp[encodedString].push_back(strs[i]);
         }
+        vector<vector<string>> ans;
         for(auto [sorted, group]:mp){
             ans.push_back(mp[sorted]);
         }
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+//         unordered_map<string,vector<string>>mp;
+//         vector<vector<string>> ans;
+//         for(int i = 0; i<strs.size();i++){
+//             string sortedItem = strs[i];
+//             sort(sortedItem.begin(), sortedItem.end());
+//             mp[sortedItem].push_back(strs[i]);
+//         }
+//         for(auto [sorted, group]:mp){
+//             ans.push_back(mp[sorted]);
+//         }
+//         return ans;
+//     }
+// };
