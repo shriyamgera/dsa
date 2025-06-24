2584e3f4c550b0ce268fa5c82e7bf27ae25b7859
shriyamgera <64017939+shriyamgera@users.noreply.github.com>
2025-06-24 23:52:05 +0530

    Time: 65 ms (81.58%) | Memory: 173.1 MB (71.51%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,36 +1,74 @@
+class Node {
+public:
+    int key;
+    int data;
+    Node* prev;
+    Node* next;
+
+    Node(int key, int val, Node* prev, Node* next) {
+        this->key = key;
+        this->data = val;
+        this->prev = prev;
+        this->next = next;
+    }
+};
+
 class LRUCache {
 public:
-    int capacity;
-    list<pair<int, int>> lruList; // {key, value}
-    unordered_map<int, list<pair<int, int>>::iterator> cache;
+    int cap;
+    int currentNodes = 0;
+    Node* head;
+    Node* tail;
+    unordered_map<int, Node*> mp;
 
     LRUCache(int capacity) {
-        this->capacity = capacity;
+        this->cap = capacity;
+        head = new Node(0, 0, nullptr, nullptr);
+        tail = new Node(0, 0, head, nullptr);
+        head->next = tail;
+    }
+
+    void moveToFront(Node* node) {
+        // Remove node from current position
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+
+        // Insert right after head
+        node->next = head->next;
+        node->prev = head;
+        head->next->prev = node;
+        head->next = node;
     }
 
     int get(int key) {
-        if (cache.find(key) == cache.end()) return -1;
-
-        // Move accessed item to front (most recently used)
-        lruList.splice(lruList.begin(), lruList, cache[key]);
-        return cache[key]->second;
+        if (mp.count(key)) {
+            Node* node = mp[key];
+            moveToFront(node);
+            return node->data;
+        }
+        return -1;
     }
 
     void put(int key, int value) {
-        if (cache.find(key) != cache.end()) {
-            // Key exists: update and move to front
-            cache[key]->second = value;
-            lruList.splice(lruList.begin(), lruList, cache[key]);
+        if (mp.count(key)) {
+            Node* node = mp[key];
+            node->data = value;
+            moveToFront(node);
         } else {
-            // New key
-            if (lruList.size() == capacity) {
-                // Remove least recently used (last item)
-                int lruKey = lruList.back().first;
-                lruList.pop_back();
-                cache.erase(lruKey);
+            if (currentNodes == cap) {
+                Node* lru = tail->prev;
+                lru->prev->next = tail;
+                tail->prev = lru->prev;
+                mp.erase(lru->key);
+                delete lru;
+                currentNodes--;
             }
-            lruList.push_front({key, value});
-            cache[key] = lruList.begin();
+
+            Node* newNode = new Node(key, value, head, head->next);
+            head->next->prev = newNode;
+            head->next = newNode;
+            mp[key] = newNode;
+            currentNodes++;
         }
     }
 };
