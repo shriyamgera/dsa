4eee96390ec22d69f04d1bb12a1e5b02e07930ce
shriyamgera <64017939+shriyamgera@users.noreply.github.com>
2025-07-02 22:37:11 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (74.20%) - LeetSync

--- 223-rectangle-area/rectangle-area.cpp
+++ 223-rectangle-area/rectangle-area.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        int areaA = (ax2-ax1) * (ay2-ay1);
-        int areaB = (bx2-bx1) * (by2-by1);
+        int areaA = (ax2-ax1)*(ay2-ay1);
+        int areaB = (bx2-bx1)*(by2-by1);
 
-        int commonWidth = max(0, min(ax2,bx2) - max(ax1,bx1));
-        int commonHeight = max(0, min(ay2,by2) - max(ay1,by1));
-        int commonArea = commonWidth * commonHeight;
-        int ans = areaA + areaB - commonArea;
+        int cx1 = max(ax1,bx1);
+        int cx2 = min(ax2,bx2);
+        int cy1 = max(ay1,by1);
+        int cy2 = min(ay2,by2);
+
+        int areaC = 0;
+        if(cx2>cx1 && cy2>cy1){
+           areaC = (cx2-cx1)*(cy2-cy1);
+        }
+
+        int ans = areaA + areaB - areaC;
+
         return ans;
+
     }
 };
