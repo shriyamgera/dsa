c90dc76ac1d309015ddb7cbd5550ba6a017f3f23
shriyamgera <64017939+shriyamgera@users.noreply.github.com>
2025-08-18 01:37:09 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (30.77%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
+    int solve(int n){
+        int count = 0;
+        for(int i=0; i<=17; i++){
+            int last = n & 1;
+            count+=last;
+            n>>=1;
+        }
+        return count;
+    }
     vector<int> countBits(int n) {
         vector<int> ans;
         for(int i=0; i<=n; i++){
-            ans.push_back(__builtin_popcount(i));
+            ans.push_back(solve(i));
         }
         return ans;
     }
