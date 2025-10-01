400407b4bd70bd4015cba604f49e56ee0a365dcb
shriyamgera <64017939+shriyamgera@users.noreply.github.com>
2025-10-02 01:21:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (10.95%) - LeetSync

--- 1642-water-bottles/water-bottles.cpp
+++ 1642-water-bottles/water-bottles.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        int ans = numBottles;
+        int ans = 0;
         int currVal = numBottles;
         int currRemainder = 0;
 
-        while(true){
-            currRemainder = currVal % numExchange;
-            currVal = currVal / numExchange;
+        while(currVal !=0){
             ans += currVal;
             currVal += currRemainder;
-            if(currVal / numExchange == 0) break;
+            currRemainder = currVal % numExchange;
+            currVal = currVal / numExchange;
         }
 
         return ans;
