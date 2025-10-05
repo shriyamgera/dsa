eff52ac088faef46b4a6d16336a09a638d4da6ee
shriyamgera <64017939+shriyamgera@users.noreply.github.com>
2025-10-05 19:30:30 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (30.97%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
-        vector<int>left(n);
-        vector<int>right(n);
+        // left and right no hold
+        // array1
+        // array2
+        // height[4] = 1;
+        // array1[4] = 2;
+        // array2[4] = 3;
 
-        int lMax = 0;
-        for(int i=0;i<n;i++){
-            left[i] = max(lMax,height[i]);
-            lMax = left[i];
+        int n = height.size();
+        vector<int>leftMax(n);
+        vector<int>rightMax(n);
+
+        leftMax[0] = height[0];
+        for(int i=1; i<n; i++){
+            leftMax[i] = max(leftMax[i-1], height[i]);
         }
 
-        int rMax = 0;
-        for(int i=n-1;i>=0;i--){
-            right[i] = max(rMax,height[i]);
-            rMax = right[i];
+        rightMax[n-1] = height[n-1];
+        for(int i=n-2; i>=0; i--){
+            rightMax[i] = max(rightMax[i+1], height[i]);
         }
 
         int ans = 0;
-        for(int i=0;i<n;i++){
-            ans+=min(left[i],right[i])-height[i];
+
+        for(int i=0; i<n; i++){
+            int maxPossibleHeight = min(leftMax[i], rightMax[i]);
+            ans += maxPossibleHeight - height[i];
         }
 
         return ans;
+
+
     }
 };
