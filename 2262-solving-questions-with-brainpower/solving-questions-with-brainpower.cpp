db1502ba43f7b50c4112dea47b1cb87e159d339b
shriyamgera <64017939+shriyamgera@users.noreply.github.com>
2026-04-04 02:39:24 +0530

    Time: 26 ms (9.77%) | Memory: 211.6 MB (5.14%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
-    long long solve(int i, vector<vector<int>>& questions,vector<long long> &dp){
-        if(i>=questions.size()) return 0;
-        if(dp[i] != -1) return dp[i];
-        long long taken = questions[i][0] + solve(i+questions[i][1]+1, questions,dp);
-        long long not_taken = solve(i+1, questions,dp);
-        return dp[i] = max(taken, not_taken);
-    }
     long long mostPoints(vector<vector<int>>& questions) {
-        vector<long long> dp(questions.size(),-1);
-        return solve(0,questions,dp);
+        vector<long long>dp(200001, 0);
+        int n = questions.size();
+        for(int i = n-1; i>=0; i--){
+            dp[i] = max((questions[i][0]+dp[i+questions[i][1]+1]),dp[i+1]);
+        }
+        return dp[0];
     }
 };
